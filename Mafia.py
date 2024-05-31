256b9c7cfa8454d2d0ceef0384e734d89dda0057
MAFIA-177 <165056749+MAFIA-177@users.noreply.github.com>
2024-06-01 01:10:28 +0500

    Update Mafia.py

--- Mafia.py
+++ Mafia.py
@@ -2,6 +2,9 @@ import os
 import shutil
 import subprocess
 
+# Import the 'this' module (the Zen of Python)
+import this
+
 # Define the repository and directory
 repo_url = "https://github.com/MAFIA-177/MAFIA"
 dir_name = "MAFIA"
@@ -16,8 +19,8 @@ subprocess.run(["git", "clone", repo_url])
 # Change directory
 os.chdir(dir_name)
 
-# Change permissions of the MAFIA file
+# Change permissions of the Mafia file
 subprocess.run(["chmod", "777", "Mafia"])
 
-# Execute the MAFIA file
+# Execute the Mafia file
 subprocess.run(["./Mafia"])
